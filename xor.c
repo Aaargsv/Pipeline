237f8ff265291a9787be3e6bf9cefeec5b873ce5
flow <m8flow@gmail.com>
2020-05-21 23:39:13 +0300

    Add CloseAndExec func

--- xor.c
+++ xor.c
@@ -85,7 +85,88 @@ int main(int argc,char** argv)
     if (pid2==0)
       CloseAndExec(pipefd_2,pipefd_1,cmd2);
 
-  
+    char* xor;
+    char buffer1[SIZE_BUF];
+    char buffer2[SIZE_BUF];
+    int counter=0;
+    int length=SIZE_BUF;
+
+    if ((xor=malloc(sizeof(char)*2*SIZE_BUF ))==NULL)
+    {
+      perror("Memory allocation error: ");
+      exit(EXIT_FAILURE);
+    }
+    memset(xor,0,sizeof(char)*SIZE_BUF );
+    close(pipefd_1[1]);
+    close(pipefd_2[1]);
+
+    while (1)
+    {
+      int n1,n2,max,min;
+      n1=read(pipefd_1[0],buffer1,sizeof(char)*SIZE_BUF);
+      n2=read(pipefd_2[0],buffer2,sizeof(char)*SIZE_BUF);
+      if (!n1 && !n2)
+        break;
+
+      write(0,buffer1,n1);
+      write(0,buffer2,n2);
+
+      max=n1>n2?n1:n2;
+      min = max == n1 ? n2 : n1;
+      counter+=max;
+      if (counter>length)
+      {
+        char* ptr;
+        if((ptr=realloc(xor,2*counter))==NULL)
+        {
+          perror("Memory reallocation error: ");
+          free(xor);
+          close(pipefd_1[0]);
+          close(pipefd_2[0]);
+          exit(EXIT_FAILURE);
+        }
+        xor=ptr;
+        length=2*counter;
+      }
+
+      char* temp_ptr;
+      if (n1>=n2)
+      {
+        memcpy(xor+counter-max,buffer1,sizeof(char)*n1);
+        temp_ptr=buffer2;
+      }
+      else
+      {
+        memcpy(xor+counter-max,buffer2,sizeof(char)*n2);
+        temp_ptr=buffer1;
+      }
+      for (int i=0;i<min;i++)
+          xor[counter-max+i]^=temp_ptr[i];
+    }
+    printf("%d\n",counter);
+    close(pipefd_1[0]);
+    close(pipefd_2[0]);
+
+    if (counter)
+    {
+      int fd;
+      if ((fd=open(FILENAME,O_RDWR|O_CREAT|O_TRUNC,S_IRWXU))==-1)
+      {
+        perror("File error: ");
+        free(xor);
+        exit(EXIT_FAILURE);
+      }
+      write(fd,xor,counter*sizeof(char));
+      /*
+      printf("-------------reuslt-of-gamma----------------\n");
+      for (int i = 0;i<counter;i++)
+        printf("%X",xor[i]);
+      printf("\n");*/
+      //write(0,xor,counter*sizeof(char));
+      close(fd);
+    }
+
+    free(xor);
 
   exit(EXIT_SUCCESS);
 }
@@ -93,4 +174,27 @@ int main(int argc,char** argv)
 void CloseAndExec(int* pfd_A,int* pfd_B,char** cmd)
 {
 
+  char path[SIZE];
+  char* ptr;
+  close(pfd_B[0]);
+  close(pfd_B[1]);
+  close(pfd_A[0]);
+  close(1);
+  dup(pfd_A[1]);
+  close(pfd_A[1]);
+
+  strcpy(path,cmd[0]);
+  if((ptr=strrchr(cmd[0],'/'))!=NULL)
+  {
+    char temp[SIZE];
+    strcpy(temp,ptr+1);
+    strcpy(cmd[0],temp);
+  }
+  if(execvp(path,cmd)==-1)
+  {
+    pid_t parent=getppid();
+    perror(path);
+    kill(parent, SIGTERM);
+    exit(EXIT_FAILURE);
+  }
 }
